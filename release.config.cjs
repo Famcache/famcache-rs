2b6acae2ea3fd38584dca2a1caf1dc5786a3af8b
Shahen Hovhannisyan <shahen.hovhannisyan.94@gmail.com>
2024-06-01 02:59:10 +0400

    fix(ci): Release include version

--- release.config.cjs
+++ release.config.cjs
@@ -5,19 +5,19 @@ module.exports = {
       "publishCmd": "cargo set-version ${nextRelease.version}"
     }],
     '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    ["@semantic-release/git", {
-      "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
-      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    }],
-    '@semantic-release/github',
+    '@semantic-release/release-notes-generator',
     [
       "semantic-release-cargo",
       {
         "allFeatures": true,
         "check": true,
       }
-    ]
+    ],
+    ["@semantic-release/git", {
+      "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
+      "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
+    '@semantic-release/github',
   ],
 };
