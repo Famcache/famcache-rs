f8b0dd67df75a0ffab8a3ef9b63b742f50f3d3e1
Shahen Hovhannisyan <shahen.hovhannisyan.94@gmail.com>
2024-06-01 03:03:08 +0400

    fix(ci): Mentioned post-release commit files

--- release.config.cjs
+++ release.config.cjs
@@ -15,7 +15,7 @@ module.exports = {
       }
     ],
     ["@semantic-release/git", {
-      "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
+      "assets": ["CHANGELOG.md", "Cargo.toml", "Cargo.lock"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
     '@semantic-release/github',
